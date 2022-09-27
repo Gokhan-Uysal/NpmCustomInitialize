6af878f1f8c6abe1dff59f9e42e768aadb22dfd5
Gökhan Uysal <guysal20@ku.edu.tr>
2022-09-27 20:57:30 +0300

    HTML  error fixed nodemon working correctly

--- nodeRun.sh
+++ nodeRun.sh
@@ -54,11 +54,11 @@ function ClearNode(){
 
 function CompleteHTML(){
     echo -e "<!DOCTYPE html>
-<html lang="en">
+<html lang=\"en\">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
     <title>Document</title>
 </head>
 <body>
